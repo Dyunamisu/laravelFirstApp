f5d31afefc864b7bedfd4a965f5dd0c1ca3118cb
Dyunamius <cleveralvin08101@gmail.com>
2023-10-24 10:43:02 +0800

    First Commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,9 +14,55 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
-    {
-        // \App\Models\User::factory(10)->create();
+    {   
+        // User::truncate();
 
+        // Category::truncate();
+
+        $user = User::factory()->create([
+            "name"=> "Alvin",
+        ]);
+        Post::factory(5)->create([
+            'user_id' => $user->id,
+        ]);
+
+        // $personal = Category::create([
+        //     'name'=>'Personal',
+        //     'slug'=> 'personal',
+        // ]);
+        // $family = Category::create([
+        //     'name'=>'Family',
+        //     'slug'=> 'family',
+        // ]);
+        // $work = Category::create([
+        //     'name'=>'Work',
+        //     'slug'=> 'work',
+        // ]);
+
+        // Post::create([
+        //     'user_id'=> $user->id,
+        //     'category_id' => $family->id,
+        //     'title' => 'My Family',
+        //     'slug'=> 'my-first-post',
+        //     'excerpt'=>'Lorem ipsum dolor sit amet consectetur adipisicing elit. ?',
+        //     'body'=>'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa ipsam libero ex animi et! Illum architecto ea, numquam id veritatis quaerat deleniti laborum rerum, repellat neque sed repudiandae asperiores. Suscipit!'
+        // ]);
+        // Post::create([
+        //     'user_id'=> $user->id,
+        //     'category_id' => $work->id,
+        //     'title' => 'My work',
+        //     'slug'=> 'my-second-post',
+        //     'excerpt'=>'Lorem ipsum dolor sit amet consectetur adipisicing elit. ?',
+        //     'body'=>'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa ipsam libero ex animi et! Illum architecto ea, numquam id veritatis quaerat deleniti laborum rerum, repellat neque sed repudiandae asperiores. Suscipit!'
+        // ]);
+        // Post::create([
+        //     'user_id'=> $user->id,
+        //     'category_id' => $personal->id,
+        //     'title' => 'My Personal',
+        //     'slug'=> 'my-third-post',
+        //     'excerpt'=>'Lorem ipsum dolor sit amet consectetur adipisicing elit. ?',
+        //     'body'=>'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa ipsam libero ex animi et! Illum architecto ea, numquam id veritatis quaerat deleniti laborum rerum, repellat neque sed repudiandae asperiores. Suscipit!'
+        // ]);
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
