e022bfe7d6e8888f9262fa105cc3949c190641f0
Dyunamius <cleveralvin08101@gmail.com>
2023-10-27 11:29:10 +0800

    before mailchimp

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ Route::get('/',[PostController::class,'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class,'show']);
 
 Route::post('posts/{post:slug}/comments', [PostCommentsController::class,'store'])->middleware('auth');;
+// api key checking 
 
 Route::get('register',[RegisterController::class,'create'])->middleware('guest');
 Route::post('register',[RegisterController::class,'store'])->middleware('guest');
